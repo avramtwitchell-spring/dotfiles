0c3e63589bf2b937e057b6ec1088a3f2fba22664
Avram Twitchell <avram.twitchell@springhealth.com>
2024-05-03 13:55:12 -0600

    Adds .p10k.zsh, misc install clean up

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Beginning setting up Dotfiles..."
 # Update package list and install zsh and neovim
 sudo apt update && sudo apt install -y zsh nodejs npm fzf
 
@@ -12,19 +13,20 @@ sudo mv ./squashfs-root $HOME/nvim
 sudo ln $HOME/nvim/usr/bin/nvim /usr/bin/nvim
 export VIMRUNTIME=$HOME/nvim/usr/share/nvim/runtime
 
-# Symlink .zshrc and .bashrc to your home directory
+# Symlink various dotfiles to home directory
 export DOTFILES=/workspaces/.codespaces/.persistedshare/dotfiles
 ln -sf $DOTFILES/.zshrc $HOME/.zshrc
 ln -sf $DOTFILES/.bashrc $HOME/.bashrc
 ln -sf $DOTFILES/.aliases $HOME/.aliases
 ln -sf $DOTFILES/.vimrc $HOME/.vimrc
+ln -sf $DOTFILES/.p10k.zsh $HOME/.p10k.zsh
+
+## Neovim configuration setup
+mkdir -p $HOME/.config/nvim
+ln -sf $DOTFILES/init.vim $HOME/.config/nvim/init.vim
 
 # Set zsh as the default shell
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
-
-# Neovim configuration setup
-mkdir -p $HOME/.config/nvim
-ln -sf $DOTFILES/init.vim $HOME/.config/nvim/init.vim
 
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -36,5 +38,4 @@ nvim --headless +PluginInstall +qall
 gem install solargraph
 sudo npm install -g typescript typescript-language-server
 
-echo "Environment setup complete."
-
+echo "Dotfile setup complete."
